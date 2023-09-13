6f07e63415b9a50db75e0aaab20360a5fa958431
liernilekeitiobhi <lierniortiz@lekeitiobhi.net>
2023-09-13 11:02:36 +0200

    database aldatu

--- config/database.js
+++ config/database.js
@@ -1,10 +1,23 @@
-const path = require('path');
+/*const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});*/
+
+
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
